a89bf4e072399614cca0733cc1d834c4236713fa
Dann97 <isc.danieljuarez@gmail.com>
2020-03-02 23:28:01 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'RobotoMono', 
+        //primaryColor: Colors.purple,
+        brightness: Brightness.dark,
+        //primaryColor: Colors.lightBlue[800],
+        //accentColor: Colors.cyan[600],
+       
+
+      ),
+      home: Scaffold(
+      appBar: AppBar(
+        title: Text('Daugther',
+          ),
+      ),
+      body:  Stack(
+      children: <Widget>[ Container(
+      decoration:  BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage("assets/bb.jpeg"
+          ), 
+      fit: BoxFit.cover,
+      ),
+    ),
+  ),
+        Center(
+          child: Container(
+            width: 500,
+            height: 100,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.white12,
+                width: 4,
+              ),
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.black38,
+            ),
+                        
+          ),
+        ),
+      Center(
+        child:  Text("Daniella Gretchen",
+        
+        style: TextStyle(
+          fontFamily:'Raleway',
+          fontWeight: FontWeight.bold,
+          fontSize:30,
+          color: Color.fromARGB(255, 255, 255,255,
+        
+        ),
+        ),
+           ),
+          )
+         ],
+        )
+       ),
+     );
+  }
+} 
